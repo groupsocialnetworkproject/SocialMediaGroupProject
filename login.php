a144130ec981d90c8a3da031d2638cf512a3da22
Joe Pearson <joepearson@Joes-MacBook-Air.local>
2019-02-26 15:17:21 +0000

    Finished the front-end of login

--- login.php
+++ login.php
@@ -11,55 +11,70 @@
 <body style="background-color: #f5f5f5;">
  <div class="container">
   <div class="row">
-    <div class="col-md-4 col-md-offset-4">
+    <div class="col-md-6 col-md-offset-3">
       <div class="page-header text-center">
-          <h1>Login Page</h1>
-      </div>
-      <ul class="nav nav-tabs" role="tablist">
-        <li class="active"><a href="#register-panel" role="tab" data-toggle="tab" class="big">Register</a>
-        </li>
-        <li><a href="#login-panel" role="tab" data-toggle="tab" class="big">Login</a>
-        </li>
-      </ul>
-      <div class="tab-content">
-        <div class="tab-pane fade in active" id="register-panel">
-          <fieldset>
-            <div class="form-group">
-              <div class="right-inner-addon">
-                <input class="form-control input-lg" placeholder="Email Address" type="text">
-              </div>
-            </div>
-            <div class="form-group">
-              <div class="right-inner-addon">
-                <input class="form-control input-lg" placeholder="Password" type="password">
-              </div>
-            </div>
-            <div class="form-group">
-              <div class="right-inner-addon">
-                <input class="form-control input-lg" placeholder="Confirm Password" id="" type="password">
-              </div>
-            </div>
-          </fieldset>
+      <h1>Login/Registration Page</h1>
+  </div>
+  <ul class="nav nav-tabs" id="myTab" role="tablist">
+    <li class="nav-item">
+      <a class="nav-link" id="login-tab" data-toggle="tab" href="#login" role="tab" aria-controls="login"
+        aria-selected="true">Login</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" id="register-tab" data-toggle="tab" href="#register" role="tab" aria-controls="register"
+        aria-selected="false">Register</a>
+    </li>
+  </ul>
+  <br>
+  <div class="tab-content" id="form-tabs">
+    <div class="tab-pane fade" id="login" role="tabpanel" aria-labelledby="login-tab">
+      <form>
+        <div class="form-group">
+          <label for="email_login">Email address</label>
+          <input type="email" class="form-control" id="email_login" aria-describedby="Email Address" placeholder="Enter email">
         </div>
-        <!-- Login -->
-        <div class="tab-pane fade" id="login-panel">
-          <fieldset>
-            <div class="form-group">
-              <div class="right-inner-addon">
-                <input class="form-control input-lg" placeholder="Email Address" type="text">
-              </div>
-            </div>
-            <div class="form-group">
-              <div class="right-inner-addon">
-                <input class="form-control input-lg" placeholder="Password" type="password">
-              </div>
-            </div>
-          </fieldset>
-          <div class=" text-center">
-            <button class="btn btn-success" style="width: 100%;">Login</button></div>
-          </div>
-      </div>
-      <br>
+        <div class="form-group">
+          <label for="password_login">Password</label>
+          <input type="password" class="form-control" id="password_login" placeholder="Password">
+        </div>
+        <button type="submit" class="btn btn-success" style="width:100%;">Login</button>
+      </form>
+    </div>
+    <div class="tab-pane fade" id="register" role="tabpanel" aria-labelledby="register-tab">
+      <form>
+        <div class="form-group">
+          <label for="first_name">First Name</label>
+          <input type="text" class="form-control" id="first_name" aria-describedby="First Name" placeholder="Enter your first name">
+        </div>
+        <div class="form-group">
+          <label for="last_name">Last Name</label>
+          <input type="text" class="form-control" id="last_name" aria-describedby="Last Name" placeholder="Enter your last name">
+        </div>
+        <div class="form-group">
+          <label for="email_login">Email address</label>
+          <input type="email" class="form-control" id="email_login" aria-describedby="Email Address" placeholder="Enter email">
+        </div>
+        <div class="form-group">
+          <label for="confirm_email_login">Confirm Email address</label>
+          <input type="email" class="form-control" id="confirm_email_login" aria-describedby="Confirm Email Address" placeholder="Confirm your email">
+        </div>
+        <div class="form-group">
+          <label for="password_login">Password</label>
+          <input type="password" class="form-control" id="password_login" placeholder="Password">
+        </div>
+        <div class="form-group">
+          <label for="confirm_password_login">Confirm Password</label>
+          <input type="password" class="form-control" id="confirm_password_login" placeholder="Confirm your password">
+        </div>
+        <button type="submit" class="btn btn-danger" style="width:100%;">Register</button>
+      </form>
+  </div>
+</div>
+    </div>
+  </div>
+  <div class="row text-center">
+    <div class="col-md-6 col-md-offset-3">
+      <p>Copyright &copy; <?php echo date('Y'); ?></p>
     </div>
   </div>
 </div>
