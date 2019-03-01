20900d1c9c0a38d3a76a3661ec24a511c25df440
Joe Pearson <joepearson@Joes-MacBook-Air.local>
2019-03-01 14:21:59 +0000

    Ooops, error with the current index page.

--- index.php
+++ index.php
@@ -1,14 +1,18 @@
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html>
 <head>
 	<title></title>
 </head>
 <body>
-	<p>hey 
+	<p>
 		<?php 
-			session_start();
-			echo $_SESSION['user_online']; 
-			echo "<br/> <a href='user_access_code.php?logout'>Logout</a>" 
+			if(isset($_SESSION['user_online'])){
+			    echo hey . $_SESSION['user_online'];
+			    echo "<br/> <a href='user_access_code.php?logout'>Logout</a>";
+			} else {
+			    echo 'why not <a href="login.php">log in</a>?';
+			}
 		?>		
 	</p>
 </body>
