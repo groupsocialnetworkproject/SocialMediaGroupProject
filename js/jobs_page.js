727c48f4e50cb7f6b1192a7e30d90aead28e6710
tomfox747 <47598118+tomfox747@users.noreply.github.com>
2019-03-13 14:24:58 +0000

    13/03/2019

--- js/jobs_page.js
+++ js/jobs_page.js
@@ -1,36 +1,120 @@
-document.getElementById("soft").addEventListener("click", function(){setField("fieBox", "Software and Technology")});
-document.getElementById("mech").addEventListener("click", function(){setField("fieBox", "Mechanical Engineering")});
-document.getElementById("elec").addEventListener("click", function(){setField("fieBox", "Electronic Engineering")});
-document.getElementById("chem").addEventListener("click", function(){setField("fieBox", "Chemical Engineering")});
+window.onload = function()
+{
+    console.log("working");
+    //MOBILE VERSION**************************************************************
+    document.getElementById("soft_mob").addEventListener("click", function () {
+        setField("fieBox_mob", "Software and Technology")
+    });
+    document.getElementById("mech_mob").addEventListener("click", function () {
+        setField("fieBox_mob", "Mechanical Engineering")
+    });
+    document.getElementById("elec_mob").addEventListener("click", function () {
+        setField("fieBox_mob", "Electronic Engineering")
+    });
+    document.getElementById("chem_mob").addEventListener("click", function () {
+        setField("fieBox_mob", "Chemical Engineering")
+    });
 
-document.getElementById("10k").addEventListener("click", function(){setField("salBox", "£0 - £10k")});
-document.getElementById("20k").addEventListener("click", function(){setField("salBox", "£10k - £20k")});
-document.getElementById("30k").addEventListener("click", function(){setField("salBox", "£20k - £30k")});
-document.getElementById("more").addEventListener("click", function(){setField("salBox", "£30k +")});
+    document.getElementById("10k_mob").addEventListener("click", function () {
+        setField("salBox_mob", "£0 - £10k")
+    });
+    document.getElementById("20k_mob").addEventListener("click", function () {
+        setField("salBox_mob", "£10k - £20k")
+    });
+    document.getElementById("30k_mob").addEventListener("click", function () {
+        setField("salBox_mob", "£20k - £30k")
+    });
+    document.getElementById("more_mob").addEventListener("click", function () {
+        setField("salBox_mob", "£30k +")
+    });
 
-document.getElementById("grad").addEventListener("click", function(){setField("typBox", "Grad Jobs")});
-document.getElementById("part").addEventListener("click", function(){setField("typBox", "Part Time Placements")});
-document.getElementById("summ").addEventListener("click", function(){setField("typBox", "Summer Placements")});
-document.getElementById("12mon").addEventListener("click", function(){setField("typBox", "12 Month Placements")});
+    document.getElementById("grad_mob").addEventListener("click", function () {
+        setField("typBox_mob", "Grad Jobs")
+    });
+    document.getElementById("part_mob").addEventListener("click", function () {
+        setField("typBox_mob", "Part Time Placements")
+    });
+    document.getElementById("summ_mob").addEventListener("click", function () {
+        setField("typBox_mob", "Summer Placements")
+    });
+    document.getElementById("12mon_mob").addEventListener("click", function () {
+        setField("typBox_mob", "12 Month Placements")
+    });
 
-document.getElementById("NY").addEventListener("click", function(){setField("locBox", "North Yorkshire")});
-document.getElementById("EY").addEventListener("click", function(){setField("locBox", "East Yorkshire")});
-document.getElementById("WY").addEventListener("click", function(){setField("locBox", "West Yorkshire")});
-document.getElementById("SY").addEventListener("click", function(){setField("locBox", "South Yorkshire")});
-document.getElementById("other").addEventListener("click", function(){setField("locBox", "pointless")});
+    document.getElementById("NY_mob").addEventListener("click", function () {
+        setField("locBox_mob", "North Yorkshire")
+    });
+    document.getElementById("EY_mob").addEventListener("click", function () {
+        setField("locBox_mob", "East Yorkshire")
+    });
+    document.getElementById("WY_mob").addEventListener("click", function () {
+        setField("locBox_mob", "West Yorkshire")
+    });
+    document.getElementById("SY_mob").addEventListener("click", function () {
+        setField("locBox_mob", "South Yorkshire")
+    });
+    document.getElementById("other_mob").addEventListener("click", function () {
+        setField("locBox_mob", "pointless")
+    });
+    
+    document.getElementById("job").addEventListener("click", function () {
+        setField("ordBox", "By Title")
+    });
+    document.getElementById("company").addEventListener("click", function () {
+        setField("ordBox", "By Company Name")
+    });
+    
 
-document.getElementById("byTitle").addEventListener("click", function(){setField("ordBox","By Title")})
-document.getElementById("byCompany").addEventListener("click", function(){setField("ordBox","By Company Name")})
 
-function setField(boxID, textIn){
-    document.getElementById(boxID).innerHTML = textIn;
+    function setField(boxID, textIn) {
+        document.getElementById(boxID).innerHTML = textIn;
+    }
+    
+     //Animated search bar
+    function expand() {
+        $(".search").toggleClass("close");
+        $(".input").toggleClass("square");
+        if ($('.search').hasClass('close')) {
+            $('input').focus();
+        } else {
+            $('input').blur();
+        }
+    }
+
+    $('button').on('click', expand);
+    //stores the windows dimensions for checking later on
+    height = $(window).height();
+    width = $(window).width();
+    //displays the mobile menu if the screen size is small enough on the page loading
+    if (width<610){
+        document.getElementById("menu").style.display = "block";
+        document.getElementById("mobile-search").style.display="block"
+    }
+    else if(width>610){
+        document.getElementById("menu").style.display = "none";
+        document.getElementById("mobile-search").style.display="none"
+    }
+    //function does the same as above but dynamically, not just on page load
+    var resize = $(window).resize(function() {
+        // This will execute whenever the window is re-sized
+        height = $(window).height(); // New height
+        width = $(window).width(); // New width
+        if (width<=610){
+            document.getElementById("menu").style.display = "block";
+            document.getElementById("mobile-search").style.display="block"
+        }
+        else if(width>610){
+            document.getElementById("menu").style.display = "none";
+            document.getElementById("mySidenav").style.width = "0";
+            document.getElementById("mobile-search").style.display="none"
+        }
+    });
+    //displays the menu when clicked
+    $('#menu').click(function(){
+        document.getElementById("mySidenav").style.width = "250px";
+    })
+    //closes menu when exit icon is clicked
+    $('#close').click(function(){
+        document.getElementById("mySidenav").style.width = "0";
+    })
 }
-/*
-function getJobsInfo(choice, location){
-    if(choice == "byJob"){
-    document.getElementById(location).innerHTML = "Job title ordering apprears here";
-    }
-    if(choice == "byCompany"){
-        document.getElementById(location).innerHTML = "Company ordering appears here";
-    }
-}*/
