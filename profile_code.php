fcd2acde40202b4224c83dc4c91da8f34b5d3fc8
LukePowell44 <35196601+LukePowell44@users.noreply.github.com>
2019-03-16 21:00:44 +0000

    Create profile_code.php

---
+++ profile_code.php
@@ -0,0 +1,32 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+/**
+ * Class for manipulating the profile page
+ */
+class Profile
+{
+	function __construct()
+	{
+		include 'database.php';
+	}
+
+	//function to get profile information
+	public function get_profile($user) {
+		$db = new Database();
+		$connection = $db->open_connection("groupproject");
+
+		$findProfile = "SELECT * FROM accounts WHERE email LIKE '%" . $user . "%' LIMIT 1";
+		
+		$resultProfile = $db->queryDb($connection, $findProfile);
+		/*if (!$result) {
+		    trigger_error('Invalid query: ' . $connection->error);
+		}*/
+		if ($resultProfile->num_rows > 0) {
+			return $profileData = mysqli_fetch_assoc($resultProfile);
+		} else {
+			$_SESSION['failMsg'] = "Email or password is incorrect!";
+		}		
+	}
+}
