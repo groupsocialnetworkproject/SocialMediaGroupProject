39db9c5764d3d524fd552a6abb79940c5e577db3
Sam Byford <32711675+Sam-Byford@users.noreply.github.com>
2019-03-17 14:06:21 +0000

    17/03/19 - Made it look sexy

--- jobs.php
+++ jobs.php
@@ -9,11 +9,11 @@
         <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-        <link type="text/css" rel="stylesheet" href="jobs_page.css?version=1">
+        <link type="text/css" rel="stylesheet" href="jobs_page.css">
   		<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
         <?php  include_once 'db_connect.php'; include_once 'job_objects.php';?>
     </head>
-    <body>
+    <body id="Jobs_Page">
     <?php include 'Header.php';?>
         <div class="row" id="space30"></div>
     	<div id="container">
@@ -147,7 +147,7 @@
                 </div>
                 <div class="col-2"></div>
                 <div class="col-2" id="average_sal">
-                    <td class="align-middle">
+                    <!--<td class="align-middle">
                                 <?php
                                     $total = 0;
                                     for($x = 0; $x < sizeof($jobs_array); $x++)
@@ -156,7 +156,7 @@
                                     }
                                     echo $total / sizeof($jobs_array)
                                 ?>
-                    </td>
+                    </td>-->
                 </div>
                 <div class="col-2"></div>
                 <div class="col-2" id="numOfCompanies">
