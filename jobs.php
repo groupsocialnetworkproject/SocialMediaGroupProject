8c8ecc3c851b0110c3d5bdee7fcc5d25987d737c
Sam Byford <32711675+Sam-Byford@users.noreply.github.com>
2019-03-18 12:44:28 +0000

    18/03/19 - More php sorted

--- jobs.php
+++ jobs.php
@@ -147,7 +147,7 @@
                 </div>
                 <div class="col-2"></div>
                 <div class="col-2" id="average_sal">
-                    <!--<td class="align-middle">
+                    <td class="align-middle">
                                 <?php
                                     $total = 0;
                                     for($x = 0; $x < sizeof($jobs_array); $x++)
@@ -156,11 +156,31 @@
                                     }
                                     echo $total / sizeof($jobs_array)
                                 ?>
-                    </td>-->
+                    </td>
                 </div>
                 <div class="col-2"></div>
                 <div class="col-2" id="numOfCompanies">
-                    <td class="align-middle">100</td>
+                    <td class="align-middle">
+                        <?php
+                        $comps_array = array();
+                        for($x = 0; $x < sizeof($jobs_array); $x++)
+                        {
+                            $check_val = 0;
+                            for($y = 0; $y < sizeof($comps_array); $y++)
+                            {
+                                if($jobs_array[$x]->getCompany() == $comps_array[$y]->getCompany())
+                                {
+                                    $check_val++;
+                                }
+                            }
+                            if($check_val == 0)
+                            {
+                                array_push($comps_array, $jobs_array[$x]);
+                            }
+                        }
+                        echo sizeof($comps_array);
+                        ?>
+                    </td>
                 </div>
                 <div class="col-1"></div>
             </div>
@@ -180,59 +200,131 @@
                 <div class="col-12" id="jobList">
                     <div>
                         <?php
+                        $url_software = "https://www.salleurl.edu/sites/default/files/styles/ample_1400/public/content/nodes/Estudio/image/5743/11072/grado-tecnicas-aplicaciones-software.jpg";
+                        $url_chemical = "https://2o7fsh4anuayrnrhe3us6v71-wpengine.netdna-ssl.com/wp-content/uploads/2017/09/banner-Chemical-Material-Sciences.jpg";
+                        $url_mechanical = "https://3fee7a1sld751eqrjr3a035t-wpengine.netdna-ssl.com/wp-content/themes/genesis-child/images/aerospace-banner.jpg";
+                        $url_electronic = "https://www.altran.com/as-content/uploads/2017/05/2-6_industrials-electronics_v2_share_1200x600.jpg";
+                        $url_bck;
                         $sql = "SELECT * FROM jobs_new";
                         $result = mysqli_query($conn, $sql);
                         $resultCheck = mysqli_num_rows($result);
                         foreach($jobs_array as $job){
-                            echo "<div class='row'>
-                                        <div class='row' id='jobad_background'></div>
-                                        <div class='col-2'><p>" . $job->getTitle() . "</p></div>
-                                        <div class='col-2'><p>" . $job->getCompany() . "</p></div>
-                                        <div class='col-2'><p>" . $job->getLocation() . "</p></div>
-                                        <div class='col-2'><p>" . $job->getSalary() . "</p></div>
-                                        <div class='col-4'><p>" . $job->getDescription() . "</p></div>
-                                    </div><hr>";
+                            if($job->getField() == "Software and Technology")
+                            {
+                                $url_bck = $url_software;
+                            }
+                            if($job->getField() == "Mechanical Engineering")
+                            {
+                                $url_bck = $url_mechanical;
+                            }
+                            if($job->getField() == "Chemical Engineering")
+                            {
+                                $url_bck = $url_chemical;
+                            }
+                            if($job->getField() == "Electronic Engineering")
+                            {
+                                $url_bck = $url_electronic;
+                            }
+                            echo "  <div class='row' style='height: 40px; background-color: #5f0776;'><div class='col-12' style='text-align: center;color:white;'><p>" . $job->getCompany() . "</p></div></div>
+                                    <div class='row' style='color:white; background-image: url(".$url_bck.");'>
+                                            <div class='col-6' style='background-color:rgba(0, 0, 0, 0.5);'>
+                                                    <div class='row'>
+                                                        <div class='col-12'><p style='font-weight: bold;text-decoration: underline;'>Job Title</p><p>" . $job->getTitle() . "</p></div>
+                                                    </div>
+                                                    <div class='row'>
+                                                        <div class='col-12'><p style='font-weight: bold;text-decoration: underline;'>Location/County</p><p>" . $job->getLocation() . "</p></div>
+                                                    </div>
+                                                    <div class='row'>
+                                                        <div class='col-12'><p style='font-weight: bold;text-decoration: underline;'>Salary</p><p>£" . $job->getSalary() . "</p></div>
+                                                    </div>
+                                            </div>
+                                            <div class='col-6' style='background-color:rgba(0, 0, 0, 0.5);'><p style='font-weight: bold;text-decoration: underline;'>Job Description</p><p>" . $job->getDescription() . "</p></div>
+                                    </div><hr><br><br>";
                         }
-                        ?>  
+                        ?>
                     </div>
                 </div>
             </div>
         </div>
         <script>
             function order() {
-                var field = document.getElementById("fieBox").textContent;
-                var type = document.getElementById("typBox").textContent;
-                var salary = document.getElementById("salBox").textContent;
-                var location = document.getElementById("locBox").textContent;
+                var field = document.getElementById("fieBox_mob").textContent;
+                var type = document.getElementById("typBox_mob").textContent;
+                var salary = document.getElementById("salBox_mob").textContent;
+                var location = document.getElementById("locBox_mob").textContent;
                 var orderBy = document.getElementById("ordBox").textContent;
                 var xhttp;
-                console.log("function was called");    
-                    $.ajax({
-                        url: 'jobs_filtering.php', //This is the current doc
-                        type: "POST",
-                        data: ({field: field, type:type, salary:salary, location:location, orderBy:orderBy}),
-                        success: function(data){
-                            var filtered_jobs = JSON.parse(data);
-                            console.log("filtering successful");
-                            
-                            var node = document.getElementById('jobList').innerHTML = "";
-                                
-                            for(i = 0; i < filtered_jobs.length; i++)
+                console.log("function was called");
+                $.ajax({
+                    url: 'jobs_filtering.php', //This is the current doc
+                    type: "POST",
+                    data: ({field: field, type:type, salary:salary, location:location, orderBy:orderBy}),
+                    success: function(data){
+                        var filtered_jobs = JSON.parse(data);
+                        console.log("filtering successful");
+
+                        var node = document.getElementById('jobList').innerHTML = "";
+                        var software = "https://www.salleurl.edu/sites/default/files/styles/ample_1400/public/content/nodes/Estudio/image/5743/11072/grado-tecnicas-aplicaciones-software.jpg";
+                        var chemical = "https://2o7fsh4anuayrnrhe3us6v71-wpengine.netdna-ssl.com/wp-content/uploads/2017/09/banner-Chemical-Material-Sciences.jpg";
+                        var mechanical = "https://3fee7a1sld751eqrjr3a035t-wpengine.netdna-ssl.com/wp-content/themes/genesis-child/images/aerospace-banner.jpg";
+                        var electronic = "https://www.altran.com/as-content/uploads/2017/05/2-6_industrials-electronics_v2_share_1200x600.jpg";
+                        var bck;
+                        for(i = 0; i < filtered_jobs.length; i++)
+                        {
+                            if(filtered_jobs[i].field == "Software and Technology")
                             {
-                                var node = document.getElementById('jobList').innerHTML += 
-                                "<div class='row'> <div class='col-2'><p>" + filtered_jobs[i].title + "</p></div><div class='col-2'><p>" + filtered_jobs[i].company + "</p></div><div class='col-2'><p>" + filtered_jobs[i].location + "</p></div><div class='col-2'><p>" + filtered_jobs[i].salary + "</p></div><div class='col-4'><p>" + filtered_jobs[i].description + "</p></div></div><hr>";
+                                bck = software;
                             }
-                            
-                            document.getElementById("numOfJobs").innerHTML = filtered_jobs.length;
-                            var total = 0;
-                            for(i = 0; i < filtered_jobs.length; i++){
-                                total = total + parseInt(filtered_jobs[i].salary);
+                            if(filtered_jobs[i].field == "Mechanical Engineering")
+                            {
+                                bck = mechanical;
                             }
-                            var output = total / filtered_jobs.length;
-                            document.getElementById("average_sal").innerHTML = output; 
+                            if(filtered_jobs[i].field == "Chemical Engineering")
+                            {
+                                bck = chemical;
+                            }
+                            if(filtered_jobs[i].field == "Electronic Engineering")
+                            {
+                                bck = electronic;
+                            }
+
+                            var node = document.getElementById('jobList').innerHTML += "<div class='row' style='height: 40px; background-color: #5f0776;'><div class='col-12' style='text-align: center;color:white;'><p>" + filtered_jobs[i].company + "</p></div></div><div class='row' style='color:white; background-image: url("+ bck +");'><div class='col-6' style='background-color:rgba(0, 0, 0, 0.6);'><div class='row'><div class='col-12'><p style='font-weight: bold;text-decoration: underline;'>Job Title</p><p>" + filtered_jobs[i].title + "</p></div></div><div class='row'><div class='col-12'><p style='font-weight: bold;text-decoration: underline;'>Location/County</p><p>" + filtered_jobs[i].location + "</p></div></div><div class='row'><div class='col-12'><p style='font-weight: bold;text-decoration: underline;'>Salary</p><p>£" + filtered_jobs[i].salary + "</p></div></div></div><div class='col-6' style='background-color:rgba(0, 0, 0, 0.6);'><p style='font-weight: bold;text-decoration: underline;'>Job Description</p><p>" + filtered_jobs[i].description + "</p></div></div><hr><br><br>";
                         }
-                    });   
-            }                
+
+                        //calc number of jobs found
+                        document.getElementById("numOfJobs").innerHTML = filtered_jobs.length;
+
+                        //calc average salary
+                        var total = 0;
+                        for(i = 0; i < filtered_jobs.length; i++){
+                            total = total + parseInt(filtered_jobs[i].salary);
+                        }
+                        var output = total / filtered_jobs.length;
+                        output = Math.round(output);
+                        document.getElementById("average_sal").innerHTML = output;
+
+                        //calc number of companies found
+                        var comps_array = [];
+                        comps_array.push(filtered_jobs[0]);
+                        for(var x = 0; x < filtered_jobs.length; x++)
+                        {
+                            var check_val = 0;
+                            for(var y = 0; y < comps_array.length; y++)
+                            {
+                                if(filtered_jobs[x].company == comps_array[y].company)
+                                {
+                                    check_val++;
+                                }
+                            }
+                            if(check_val == 0)
+                            {
+                                comps_array.push(comps_array[x]);
+                            }
+                        }
+                        document.getElementById("numOfCompanies").innerHTML = comps_array.length;
+                    }
+                });
+            }
         </script>
     <script src="jobs_page.js?version=1"></script>
     </body>
